a3fb11169e530ed1f8cfed5aaf12ccfe10226aef
Andy Mepham <andymeps@yahoo.com>
2016-08-02 22:40:05 +0100

    Made some minor changes, testing branch merge.

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ let app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 3000);
+
+console.log('Server started at: localhost:' + (process.env.PORT || 3000));
